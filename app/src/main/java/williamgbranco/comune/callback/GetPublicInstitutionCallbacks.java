ccbe9e8ffea78c2c8b720244431dbe9e81ec9140
wgbranco <williamgbranco@gmail.com>
2016-02-15 00:15:34 -0200

    Initial commit

---
+++ app/src/main/java/williamgbranco/comune/callback/GetPublicInstitutionCallbacks.java
@@ -0,0 +1,11 @@
+package williamgbranco.comune.callback;
+
+import williamgbranco.comune.institution.PublicInstitution;
+
+/**
+ * Created by William Gomes de Branco on 11/10/2015.
+ */
+public interface GetPublicInstitutionCallbacks
+{
+    void done(PublicInstitution returnedInstitution);
+}
